f248871b51f0e1181271d9ebc0ea547cb2423078
Hewan2568 <hewitegenie@gmail.com>
2021-06-19 08:04:06 -0700

    my first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - returns void
- *
- * Return: returns 0
- */
+* main - returns void
+*
+* Return: returns 0
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-
