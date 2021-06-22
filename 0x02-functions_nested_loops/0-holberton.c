1c33f2a26adf72331201f962048984c693b28086
Hewan2568 <hewitegenie@gmail.com>
2021-06-22 09:54:21 -0700

    my first commit

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
+
 /**
-* main - prints the string "Holberton" from a character array.
+* main - function to print
 *
-* Return: 0 on success.
+* Return: 0
 */
 int main(void)
 {
-char c[] = "Holberton";
-int i;
+char holberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+unsigned int c;
 
-for (i = 0; i < 9; i++)
+for (c = 0; c < sizeof(holberton); c++)
 {
-_putchar(c[i]);
+_putchar(holberton[c]);
 }
 _putchar('\n');
 return (0);
